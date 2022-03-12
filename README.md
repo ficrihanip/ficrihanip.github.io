195ce1a18cb68daf01452cce125405ccfddabc13
ficrihanip <101483601+ficrihanip@users.noreply.github.com>
2022-03-13 06:48:59 +0700

    upload file tugas rwd 2

---
+++ README.md
@@ -0,0 +1,20 @@
+## _User Interface Tugas 2 Responsive Web Desain_
+
+>Ficri Hanip
+>STT Terpadu Nurul Fikri
+
+
+### Deskripsi tugas :
+* Membuat component :
+    - Banner
+    - Navbar
+    - Accordion/Tabs
+    - Image
+    - Tables
+* Content wajib :
+    - Profile
+    - Pendidikan
+    - Sertifikasi/Portfolio Project
+    - Gallery
+    - Embed Video Youtube
+* Menggunakan template varian bootstrap 5.1.3, minimal versi 4.6
---
+++ script.js
@@ -0,0 +1,115 @@
+$(document).ready(function () {
+    /**
+     * Navbar :
+     * - Ketika diklik akan menghapus class active sebelumnya 
+     * dan menambahkan class active pada class saat.
+     */
+    $('.nav-link').click(function () {
+        $('.nav-link.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    /**
+     * Banner :
+     * - Perulangan menambahkan element image banner pada
+     * dom.
+     */
+    let firstBanner = $('.carousel-item.active');
+    for (let i = 2; i <= 14; i++) firstBanner.before(`<div class="carousel-item" data-bs-interval="100"><img src="./asset/image/banner/img_banner-${i}.png" class="d-block w-100" alt="banner-${i}"></div>`);
+    for (let i = 14; i >= 2; i--) firstBanner.before(`<div class="carousel-item" data-bs-interval="100"><img src="./asset/image/banner/img_banner-${i}.png" class="d-block w-100" alt="banner-${i}"></div>`);
+
+    /**
+     * Profil :
+     * - Menghilangkan efek filter grayscale pada css
+     */
+    $('.image-profil').mouseover(function () {
+        $(this).css({
+            'filter': 'none'
+        });
+    }).mouseout(function () {
+        $(this).css({
+            'filter': 'grayscale(1)'
+        });
+    });
+
+    /**
+     * Sertif :
+     * - Perulangan menambahkan element accordion pada dom.
+     */
+    const dataArrSertif = ['PROGATE - HTML&CSS', 'PROGATE - JAVASCRIPT', 'PROGATE - SQL', 'PROGATE - GIT', 'PROGATE - COMMAND LINE', 'MSIB PROGATE BATCH 1 2020 - INTRO TO WEB DEVELOPMENT', 'MSIB PROGATE BATCH 1 2020 - JUNIOR WEB DEVELOPER'];
+    const parentDivSertif = $('section.section.sertif div.container div.row.mb-4');
+    $.each(dataArrSertif, function (index, value) {
+        let a = index + 1;
+        $(parentDivSertif).after(`<div class="row">
+                <div class="col-md-12 mb-2 mb-md-2 mb-lg-2 mb-xl-2 mb-xxl-2">
+                    <div class="accordion" id="accordionPanelsStayOpenExample">
+                        <div class="accordion-item alert-dark">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button alert-dark fw-bold collapsed" type="button"
+                                    data-bs-toggle="collapse" data-bs-target="#panel-${a}" aria-expanded="true"
+                                    aria-controls="panel-${a}">
+                                    ${value}
+                                </button>
+                            </h2>
+                            <div id="panel-${a}" class="accordion-collapse collapse" aria-labelledby="headingOne">
+                                <div class="accordion-body">
+                                    <!-- card here -->
+                                    <img class="image-sertif" src="./asset/image/sertifikasi/img_${a}.webp" alt=image-${a}">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`);
+    });
+
+    /**
+     * Galeri :
+     * - Perulangan menambahkan element card pada dom.
+     */
+    const parentDivGaleri = $('section.section.galeri div.container');
+    const dataArrGaleri = {
+        "row1": [{
+            "jenisWeb": "Web Aplikasi",
+            "namaWeb": "Bikinin Password1",
+            "link": "https://bangef.github.io/bikin-password.gihub.io/"
+        }, {
+            "jenisWeb": "Web Aplikasi",
+            "namaWeb": "Bikinin Password2",
+            "link": "https://bangef.github.io/bikin-password.gihub.io/"
+        }, {
+            "jenisWeb": "Web Aplikasi",
+            "namaWeb": "Bikinin Password3",
+            "link": "https://bangef.github.io/bikin-password.gihub.io/"
+        }],
+        "row2": [{
+            "jenisWeb": "Web Aplikasi",
+            "namaWeb": "Bikinin Password",
+            "link": "https://bangef.github.io/bikin-password.gihub.io/"
+        }, {
+            "jenisWeb": "Web Aplikasi",
+            "namaWeb": "Bikinin Password",
+            "link": "https://bangef.github.io/bikin-password.gihub.io/"
+        }, {
+            "jenisWeb": "Web Aplikasi",
+            "namaWeb": "Bikinin Password",
+            "link": "https://bangef.github.io/bikin-password.gihub.io/"
+        }]
+    };
+    // $.each(dataArrGaleri.row1, function (i, currentVal) {
+    //     $.each(currentVal, function (key, val) {
+    //         let a = i + 1;
+    //         if (a % 3 == 0) {
+    //             $(parentDivGaleri).append(`<div class="row ${i + 1}"></div>`);
+    //         }
+    //         // if (i % 3 == 0) {
+    //         //     console.log(val);
+
+    //         // }
+    //         // //     $('section.section.galeri div.container div.row').append(`<div class="col-md-4 col-sm-4">
+
+    //     });
+
+    // });
+});
+//             
